d66981b9fe77c1db7e929eefac25a59c1f007fab
Kamil Musiał <kmusial40@gmail.com>
2019-10-05 15:12:09 +0200

    rework

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import App from './app';
 import AuthenticationController from './authentication/auth.controller';
+import ReviewController from './reviews/review.controller';
 
 
 const app = new App([
+  new ReviewController(),
   new AuthenticationController()
 ]);
 
